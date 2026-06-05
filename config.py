b6fae5e541d6b10944d68eaa04ed2f1c5fb621f6
shadowking <198526556+prajwal087@users.noreply.github.com>
2026-06-05 17:58:50 +0400

    Configuration
    This is configuration for database

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config:
+    SECRET_KEY = 'random-secret-key'
+    MYSQL_HOST = 'localhost'
+    MYSQL_PORT = 3306
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'secops_hub'
