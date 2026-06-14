f614bee1f70527c876d7c09840d0b07b9c70dc9f
shadowking <198526556+prajwal087@users.noreply.github.com>
2026-06-14 19:51:00 +0545

    Create Playbook model with attributes

---
+++ SecOpsHub/app/models/playbook.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+class Playbook:
+    def __init__(self, id, name, description, steps, created_at):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.steps = steps
+        self.created_at = created_at
